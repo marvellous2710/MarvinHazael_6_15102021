7869ee279ffb78adc52c59c941328056061ab77f
marvellous2710 <80904897+marvellous2710@users.noreply.github.com>
2021-11-18 12:00:09 +0100

    getAll

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -58,3 +58,9 @@ exports.getOneSauce = (req, res, next) => {
       .then(sauce => res.status(200).json(sauce))
       .catch(error => res.status(404).json({ error }));
 }
+
+exports.getAllSauces = (req, res, next) => {
+    Sauce.find()
+      .then(sauces => res.status(200).json(sauces))
+      .catch(error => res.status(400).json({ error }));
+}
