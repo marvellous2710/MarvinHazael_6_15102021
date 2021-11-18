76a9bb7f98554eba6d641fca14880f3dcf3ef081
marvellous2710 <80904897+marvellous2710@users.noreply.github.com>
2021-11-18 11:59:33 +0100

    getOne

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -51,3 +51,10 @@ exports.deleteSauce =  (req, res, next) => {
       })
       .catch(error => res.status(500).json({ error }));
 }
+
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+      .then(sauce => res.status(200).json(sauce))
+      .catch(error => res.status(404).json({ error }));
+}
