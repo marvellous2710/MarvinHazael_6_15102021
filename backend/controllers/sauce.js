6c56b10d8e7d2ce47a0ea858ef4c2244fb4c8156
marvellous2710 <80904897+marvellous2710@users.noreply.github.com>
2021-11-18 11:57:31 +0100

    modification sauce

---
+++ backend/controllers/sauce.js
@@ -0,0 +1,38 @@
+const Sauce       = require('../models/sauce');
+const fs          = require('fs');
+const { request } = require('http');
+
+exports.createSauce = (req, res, next) => {
+    const sauceObject = JSON.parse(req.body.sauce);
+    delete sauceObject._id;
+    const sauce = new Sauce({
+        ...sauceObject,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    });
+    sauce.save()
+        .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
+        .catch(error => res.status(500).json({ error }));
+}
+
+
+exports.modifySauce =  (req, res, next) => {
+    //supprimer l'image de la sauce modifée
+    Sauce.findOne({ _id: req.params.id })
+    .then(sauce => {
+      const filename = sauce.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        const sauceObject = req.file ?// ? = si il existe...
+        {
+          ...JSON.parse(req.body.sauce),
+          imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        } : { ...req.body };
+
+
+      Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
+            .catch(error => res.status(500).json({ error }));
+      });
+    })
+    .catch(error => res.status(500).json({ error }));
+
+}
