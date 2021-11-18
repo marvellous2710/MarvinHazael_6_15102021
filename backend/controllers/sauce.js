6ff08f18f25b0c94a30ec4f107b273b8bd6365d8
marvellous2710 <80904897+marvellous2710@users.noreply.github.com>
2021-11-18 11:58:49 +0100

    delete sauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -36,3 +36,18 @@ exports.modifySauce =  (req, res, next) => {
     .catch(error => res.status(500).json({ error }));
 
 }
+
+exports.deleteSauce =  (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+      .then(sauce => {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          Sauce.deleteOne({ _id: req.params.id })
+          .then(() => {
+            res.status(200).json({ message: 'Sauce supprimée !'})
+          })
+          .catch(error => res.status(400).json({ error }));
+        });
+      })
+      .catch(error => res.status(500).json({ error }));
+}
